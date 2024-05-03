2245e108fc4e86fa2c4b41fb4f4498bb15880f6e
marlone000 <142364606+marlone000@users.noreply.github.com>
2024-05-03 20:07:07 -0300

    saved

---
+++ admin.php
@@ -0,0 +1,4 @@
+<?php include "header.php"; ?>
+    <h2>Ambiente adiministrador</h2>
+    <a href="form-inserir.php" class="btn btn-primary">cadastrar pokemon</a>
+<?php include "footer.php"; ?>
---
+++ form-inserir.php
@@ -0,0 +1,6 @@
+<?php include "header.php" ;?>
+    <h2>CADASTRAR UM NOVO POKEMON</h2>
+    <form method="post" action="salvar.php" enctype="multipart/form-data">
+        
+    </form>
+<?php include "footer.php" ?>
